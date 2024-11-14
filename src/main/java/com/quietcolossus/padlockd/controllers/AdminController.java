5e56d3d5402f6de973d9a8908cc45717ae1f5b89
Colin <colinm@rspndr.io>
2024-11-14 16:46:29 -0500

    feat: add user service and admin controller

--- src/main/java/com/quietcolossus/padlockd/controllers/AdminController.java
+++ src/main/java/com/quietcolossus/padlockd/controllers/AdminController.java
@@ -1,13 +1,37 @@
 package com.quietcolossus.padlockd.controllers;
 
+import com.quietcolossus.padlockd.dtos.UserDto;
+import com.quietcolossus.padlockd.models.User;
 import com.quietcolossus.padlockd.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
     @Autowired
     UserService userService;
+
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @PutMapping("update-role")
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId,
+                                                 @RequestParam String roleName) {
+        userService.updateUserRole(userId, roleName);
+        return ResponseEntity.ok("User role updated");
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
 }
